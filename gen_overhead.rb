6950c8cc0fb919bd1f608f8f66e4b923f0488cb8
jbmilgrom <jbmilgrom@gmail.com>
2014-09-24 14:29:45 -0400

    Remove old code

--- gen_overhead.rb
+++ gen_overhead.rb
@@ -1,11 +1,6 @@
 $generation =[9, 10, 21, 20, 7, 11, 4, 15, 7, 7, 14, 5, 20, 6, 29, 8, 11, 19, 18, 22, 29, 14, 27, 17, 6, 22, 12, 18, 18, 30]
 $overhead =[21, 16, 19, 26, 26, 7, 1, 8, 17, 14, 15, 25, 20, 3, 24, 5, 28, 9, 2, 14, 9, 25, 15, 13, 15, 9, 6, 20, 27, 22]
 
-# puts $generation.length
-# puts $overhead.length
-
-s_overhead = $overhead.sort
-
 # sorting by OVERHEAD values
 # for larger groups of cards, initial generation costs play no meaningful role 
 # in any max-min calculation
